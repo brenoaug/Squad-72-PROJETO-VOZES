4784fcd3b09ca05441a94d9938c6a68c77328295
breno augusto <breaugustocp@outlook.com>
2025-06-13 17:59:21 -0300

    começando o spring

---
+++ vozes-api/src/main/java/br/org/recode/vozes_api/VozesApiApplication.java
@@ -0,0 +1,13 @@
+package br.org.recode.vozes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VozesApiApplication.class, args);
+	}
+
+}
---
+++ vozes-api/src/test/java/br/org/recode/vozes_api/VozesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.recode.vozes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VozesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
