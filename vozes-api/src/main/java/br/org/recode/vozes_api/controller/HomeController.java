35866dcd3a6627a5e058a91c71ce5bd653a4e18c
breno augusto <breaugustocp@outlook.com>
2025-06-14 12:20:52 -0300

    conectando ao banco de dados e permitindo acesso a rota raiz do projeto, excluindo o spring security no VozesApiAplication e testando se está funcionando, retornando uma mensagem na rota '/'

---
+++ vozes-api/src/main/java/br/org/recode/vozes_api/controller/HomeController.java
@@ -0,0 +1,12 @@
+package br.org.recode.vozes_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "estou funcionando";
+    }
+}
