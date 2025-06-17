7c79f205c88a9bcba4348168010151a5aebb0885
breno augusto <breaugustocp@outlook.com>
2025-06-17 20:32:07 -0300

    página de profissionais criada

--- vozes-react/src/pages/Profissionais.jsx
+++ vozes-react/src/pages/Profissionais.jsx
@@ -3,6 +3,7 @@ import { Card, Button, Container, Row, Col, Badge } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import psicologo from "../assets/psi.jpg";
 import "../style/Profissionais.css";
+import axios from "axios";
 
 function Profissionais() {
   const [dark, setDark] = useState(
@@ -28,7 +29,7 @@ function Profissionais() {
     >
       <Container className="align-items-center p-0">
         <Card
-          className={`container-profissionais container-fluid gap-2 d-flex flex-row flex-wrap ${
+          className={`container-profissionais d-flex flex-row flex-wrap gap-3 p-5 ${
             dark ? "bg-secondary text-light" : "bg-light text-dark"
           }`}
         >
@@ -36,7 +37,66 @@ function Profissionais() {
             className={`card-profissionais text-start p-0 col-12 col-md-4 gap-1 ${
               dark ? "bg-secondary text-light" : "bg-light text-dark"
             }`}
-            style={{ flex: 1 }}
+
+          >
+            <Card.Img
+              variant="top"
+              src={psicologo}
+              alt="Psicologo"
+              style={{}}
+            />
+            <Badge
+              className="badge-profissionais"
+              style={{
+                position: "absolute",
+                top: "12px",
+                right: "12px",
+                zIndex: 1,
+              }}
+            >
+              Profissão
+            </Badge>
+            <Card.Body>
+              <Card.Title>Nome do Profissional</Card.Title>
+              <Card.Text>
+                <i class="bi bi-geo-alt-fill"></i>Cidade, Estado
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card
+            className={`card-profissionais text-start p-0 col-12 col-md-4 gap-1 ${
+              dark ? "bg-secondary text-light" : "bg-light text-dark"
+            }`}
+          >
+            <Card.Img
+              variant="top"
+              src={psicologo}
+              alt="Psicologo"
+              style={{}}
+            />
+            <Badge
+              className="badge-profissionais"
+              style={{
+                position: "absolute",
+                top: "12px",
+                right: "12px",
+                zIndex: 1,
+              }}
+            >
+              Profissão
+            </Badge>
+            <Card.Body>
+              <Card.Title>Nome do Profissional</Card.Title>
+              <Card.Text>
+                <i class="bi bi-geo-alt-fill"></i>Cidade, Estado
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card><Card
+            className={`card-profissionais text-start p-0 col-12 col-md-4 gap-1 ${
+              dark ? "bg-secondary text-light" : "bg-light text-dark"
+            }`}
           >
             <Card.Img
               variant="top"
