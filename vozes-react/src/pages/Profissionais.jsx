0588bddb7f45a99ca805c36c9aa8c507f3a3d3b9
breno augusto <breaugustocp@outlook.com>
2025-06-17 22:42:40 -0300

    get mapeado, lista de profissionais funcionando

--- vozes-react/src/pages/Profissionais.jsx
+++ vozes-react/src/pages/Profissionais.jsx
@@ -3,9 +3,10 @@ import { Card, Button, Container, Row, Col, Badge } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import psicologo from "../assets/psi.jpg";
 import "../style/Profissionais.css";
-import axios from "axios";
+import api from "../services/api";
 
 function Profissionais() {
+  const [profissionais, setProfissionais] = useState([]);
   const [dark, setDark] = useState(
     document.body.classList.contains("dark-theme")
   );
@@ -21,6 +22,15 @@ function Profissionais() {
     return () => observer.disconnect();
   }, []);
 
+  useEffect(() => {
+    api.get('api/profissionais')
+      .then(response => {
+        console.log('Dados recebidos:', response.data);
+        setProfissionais(response.data);
+      })
+      .catch(error => console.error('Erro na requisição:', error));
+  }, []);
+
   return (
     <main
       className={`text-center text-lg-start pt-5 pb-5 ${
@@ -33,37 +43,7 @@ function Profissionais() {
             dark ? "bg-secondary text-light" : "bg-light text-dark"
           }`}
         >
-          <Card
-            className={`card-profissionais text-start p-0 col-12 col-md-4 gap-1 ${
-              dark ? "bg-secondary text-light" : "bg-light text-dark"
-            }`}
-
-          >
-            <Card.Img
-              variant="top"
-              src={psicologo}
-              alt="Psicologo"
-              style={{}}
-            />
-            <Badge
-              className="badge-profissionais"
-              style={{
-                position: "absolute",
-                top: "12px",
-                right: "12px",
-                zIndex: 1,
-              }}
-            >
-              Profissão
-            </Badge>
-            <Card.Body>
-              <Card.Title>Nome do Profissional</Card.Title>
-              <Card.Text>
-                <i class="bi bi-geo-alt-fill"></i>Cidade, Estado
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+          {profissionais.map(profissional => (
           <Card
             className={`card-profissionais text-start p-0 col-12 col-md-4 gap-1 ${
               dark ? "bg-secondary text-light" : "bg-light text-dark"
@@ -87,42 +67,15 @@ function Profissionais() {
               Profissão
             </Badge>
             <Card.Body>
-              <Card.Title>Nome do Profissional</Card.Title>
+              <Card.Title>{profissional.nome}</Card.Title>
               <Card.Text>
-                <i class="bi bi-geo-alt-fill"></i>Cidade, Estado
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card><Card
-            className={`card-profissionais text-start p-0 col-12 col-md-4 gap-1 ${
-              dark ? "bg-secondary text-light" : "bg-light text-dark"
-            }`}
-          >
-            <Card.Img
-              variant="top"
-              src={psicologo}
-              alt="Psicologo"
-              style={{}}
-            />
-            <Badge
-              className="badge-profissionais"
-              style={{
-                position: "absolute",
-                top: "12px",
-                right: "12px",
-                zIndex: 1,
-              }}
-            >
-              Profissão
-            </Badge>
-            <Card.Body>
-              <Card.Title>Nome do Profissional</Card.Title>
-              <Card.Text>
-                <i class="bi bi-geo-alt-fill"></i>Cidade, Estado
+                <i class="bi bi-geo-alt-fill"></i>
+                {profissional.localizacao}
               </Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
+          ))}
         </Card>
       </Container>
     </main>
