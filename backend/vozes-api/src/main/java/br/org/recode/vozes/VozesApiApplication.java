f1eff12b8d8b9b485b44f38d9a088b5644c20671
breno augusto <breaugustocp@outlook.com>
2025-07-05 10:10:36 -0300

    refactor(api): Otimiza a estrutura dos JSONs de resposta da API

--- backend/vozes-api/src/main/java/br/org/recode/vozes/VozesApiApplication.java
+++ backend/vozes-api/src/main/java/br/org/recode/vozes/VozesApiApplication.java
@@ -3,8 +3,11 @@ package br.org.recode.vozes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class VozesApiApplication {
 
     public static void main(String[] args) {
